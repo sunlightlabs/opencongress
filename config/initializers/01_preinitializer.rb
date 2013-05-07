c612163cbe067e06fe45c91c893068709acc9950
Dan Drinkard <dan.drinkard@gmail.com>
2013-05-06 22:07:24 -0400

    load sorted initializers in order rather than requiring from dependents

--- config/initializers/01_preinitializer.rb
+++ config/initializers/01_preinitializer.rb
@@ -4,12 +4,12 @@
 # rescue LoadError
 #   raise "Could not load the bundler gem. Install it with `gem install bundler`."
 # end
-# 
+#
 # if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
 #   raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
 #    "Run `gem install bundler` to upgrade."
 # end
-# 
+#
 # begin
 #   # Set up load paths for all bundled gems
 #   ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)

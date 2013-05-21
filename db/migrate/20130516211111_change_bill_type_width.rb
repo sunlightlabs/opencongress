f377dc75ddda1366c77e29a5d35f3054ea4687fc
Drew Vogel <dvogel@sunlightfoundation.com>
2013-05-21 14:50:49 -0400

    Added data migration for existing schema migration.

--- db/migrate/20130516211111_change_bill_type_width.rb
+++ db/migrate/20130516211111_change_bill_type_width.rb
@@ -1,9 +1,34 @@
 class ChangeBillTypeWidth < ActiveRecord::Migration
+  @@govtrack_to_thomas = [
+    ["hc", "hconres"],
+    ["hr", "hres"],     # It's paramount that we convert hr => hres before
+    ["h", "hr"],        # converting h => hr, else we would map both to hres
+    ["hj", "hjres"],
+    ["sj", "sjres"],
+    ["sc", "sconres"],
+    ["sr", "sres"]
+    # We can ignore "s" to "s"
+  ]
+
   def self.up
     change_column :bills, :bill_type, :string, :limit => 7
+
+    @@govtrack_to_thomas.each do |govtrack_abbr, thomas_abbr|
+      Bill.where(:bill_type => govtrack_abbr).each do |bill|
+        bill.bill_type = thomas_abbr
+        bill.save!
+      end
+    end
   end
 
   def self.down
+    @@govtrack_to_thomas.each do |govtrack_abbr, thomas_abbr|
+      Bill.where(:bill_type => thomas_abbr).each do |bill|
+        bill.bill_type = govtrack_abbr
+        bill.save!
+      end
+    end
+
     change_column :bills, :bill_type, :string, :limit => 2
   end
 end

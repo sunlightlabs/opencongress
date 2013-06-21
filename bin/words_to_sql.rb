4547339dc4d3aade60701b4a2b3d26ea52e4c0c3
Drew Vogel <dvogel@sunlightfoundation.com>
2013-06-21 18:26:30 -0400

    Rearranged words_to_sql.rb for efficiency.

--- bin/words_to_sql.rb
+++ bin/words_to_sql.rb
@@ -7,20 +7,24 @@ end
 
 SubjectRelation.transaction do
   CSV(File.open('words.txt', 'r')).each_with_index do |record, idx|
-    if idx > 0
-      a, b, cnt = record
+    next if idx == 0 # Throw away the header
 
-      rel_ident = { :subject_id => a.to_i,
-                    :related_subject_id => b.to_i }
-      rel = SubjectRelation.where(rel_ident).first
-      if rel.nil?
-        rel = SubjectRelation.new(rel_ident)
-      end
-      rel.relation_count = cnt
-      rel.save!
+    a = record[0].to_i
+    b = record[1].to_i
+    cnt = record[2].to_i
 
-      OCLogger.log "Processed #{idx}" if idx % 10000 == 0
+    next if cnt <= 100 # Who cares about subjects that appear togther so rarely?
+
+    rel_ident = { :subject_id => a.to_i,
+                  :related_subject_id => b.to_i }
+    rel = SubjectRelation.where(rel_ident).first
+    if rel.nil?
+      rel = SubjectRelation.new(rel_ident)
     end
+    rel.relation_count = cnt
+    rel.save! if rel.changed?
+
+    OCLogger.log "Processed #{idx}" if idx % 10000 == 0
   end
 end
 

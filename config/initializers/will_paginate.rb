2a71c3f09fbf783ea798ccefb1788fde6e56e202
Dan Drinkard <dan.drinkard@gmail.com>
2013-06-26 18:04:08 -0400

    fix array pagination sitewide

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,3 +1,5 @@
-WillPaginate::ViewHelpers.pagination_options[:renderer] = 'SpanLinkRenderer'      
+require 'will_paginate/array'
+
+WillPaginate::ViewHelpers.pagination_options[:renderer] = 'SpanLinkRenderer'
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Previous'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Next'

d75ad60ba6cc8f75bae8c0d6138a105d3d6b7923
Dan Drinkard <dan.drinkard@gmail.com>
2013-07-01 16:08:33 -0400

    disable mail deliveries on staging until we get dns access

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,8 +10,8 @@ OpenCongress::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :postmark
 
   # Enable threaded mode

7ab0e1a64e19307bbe08fb47723617575e8f03f4
Drew Vogel <dvogel@sunlightfoundation.com>
2013-08-15 16:33:51 -0400

    Removed duplicate cache_store setting in development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,6 @@ OpenCongress::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.cache_store = :mem_cache_store, 'localhost:11211'
-
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = false

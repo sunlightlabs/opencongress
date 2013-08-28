dae152703622ce245ba91271c26bcf439274b1cb
Drew Vogel <dvogel@sunlightfoundation.com>
2013-08-28 11:43:31 -0400

    Completely removed FriendObserver.before_destroy.

--- app/models/friend_observer.rb
+++ app/models/friend_observer.rb
@@ -4,16 +4,6 @@ class FriendObserver < ActiveRecord::Observer
     UserNotifier.friend_notification(friend).deliver unless friend.confirmed == true
   end
 
-  def before_destroy(friend)
-   unless @reciprical_flag
-    reciprical = Friend.find_by_friend_id_and_user_id(friend.user_id, friend.friend_id)
-    UserNotifier.friendship_broken_notification(reciprical).deliver if friend.confirmed == true
-#    UserNotifier.friend_rejected_notification(reciprical).deliver if friend.confirmed == false
-    @reciprical_flag = true
-    reciprical.destroy if reciprical
-   end
-  end
-
   def after_save(friend)
     if friend.recently_confirmed?
       UserNotifier.friend_confirmed_notification(friend).deliver

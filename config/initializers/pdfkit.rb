c021b3e992563c6990789849067f138ad8378f47
Dan Drinkard <dan.drinkard@gmail.com>
2013-08-29 12:10:45 -0400

    let wkhtmltopdf run headless

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,4 +1,5 @@
 PDFKit.configure do |config|
+  config.wkhtmltopdf = 'xvfb-run wkhtmltopdf' if `which xvfb-run`
   config.default_options = {
     :page_size => 'Letter',
     :print_media_type => true

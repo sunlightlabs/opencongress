e1aa12709eb5b8a2496daac889dd9b8e18c5ffb3
Dan Drinkard <dan.drinkard@gmail.com>
2013-08-30 18:02:45 -0400

    update formageddon & change check for wkhtmltopdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/bin/wkhtmltopdf' if `which /bin/wkhtmltopdf`
+  config.wkhtmltopdf = '/bin/wkhtmltopdf' unless `which /bin/wkhtmltopdf`.empty?
   config.default_options = {
     :page_size => 'Letter',
     :print_media_type => true

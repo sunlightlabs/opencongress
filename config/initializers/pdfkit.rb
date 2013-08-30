934c7921795b68e8be8cd64d3385e9450063e345
Dan Drinkard <dan.drinkard@gmail.com>
2013-08-29 22:16:58 -0400

    look specifically in /bin for wkhtmltopdf (fix this plz) update formageddon

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = 'xvfb-run wkhtmltopdf' if `which xvfb-run`
+  config.wkhtmltopdf = '/bin/wkhtmltopdf' if `which /bin/wkhtmltopdf`
   config.default_options = {
     :page_size => 'Letter',
     :print_media_type => true

d1cd2ca099e84c6b572213a589bff2c0ca93a484
Drew Vogel <dvogel@sunlightfoundation.com>
2013-09-05 14:46:27 -0400

    Added indexes to speed up the abstain vote counting query.

---
+++ db/migrate/20130905183202_create_id_and_session_index_on_bills.rb
@@ -0,0 +1,9 @@
+class CreateIdAndSessionIndexOnBills < ActiveRecord::Migration
+  def self.up
+    add_index :bills, [:id, :session]
+  end
+
+  def self.down
+    remove_index :bills, [:id, :session]
+  end
+end
---
+++ db/migrate/20130905183306_create_roll_call_id_and_vote_index_on_roll_call_votes.rb
@@ -0,0 +1,9 @@
+class CreateRollCallIdAndVoteIndexOnRollCallVotes < ActiveRecord::Migration
+  def self.up
+    add_index :roll_call_votes, [:roll_call_id, :vote]
+  end
+
+  def self.down
+    remove_index :roll_call_votes, [:roll_call_id, :vote]
+  end
+end

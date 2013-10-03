41a77bf183780df376619f313495eb1896c53f9a
Dan Drinkard <dan.drinkard@gmail.com>
2013-10-03 13:49:03 -0400

    html/js for header redesign

--- public/javascripts/star-rating/jquery.rating.js
+++ public/javascripts/star-rating/jquery.rating.js
@@ -19,21 +19,21 @@
 /*# AVOID COLLISIONS #*/
 ;if(window.jQuery) (function($){
 /*# AVOID COLLISIONS #*/
-	
+
 	// IE6 Background Image Fix
 	if ($.browser.msie) try { document.execCommand("BackgroundImageCache", false, true)} catch(e) { }
 	// Thanks to http://www.visualjquery.com/rating/rating_redux.html
-	
+
 	// default settings
 	$.rating = {
 		cancel: 'Cancel Rating',   // advisory title for the 'cancel' link
 		cancelValue: '',           // value to submit when user click the 'cancel' link
 		split: 0,                  // split the star into how many parts?
-		
+
 		// Width of star image in case the plugin can't work it out. This can happen if
 		// the jQuery.dimensions plugin is not available OR the image is hidden at installation
 		starWidth: 16,
-		
+
 		//NB.: These don't need to be defined (can be undefined/null) so let's save some code!
 		//half:     false,         // just a shortcut to settings.split = 2
 		//required: false,         // disables the 'cancel' button so user can only select one of the specified values
@@ -41,7 +41,7 @@
 		//focus:    function(){},  // executed when stars are focused
 		//blur:     function(){},  // executed when stars are focused
 		//callback: function(){},  // executed when a star is clicked
-		
+
 		// required properties:
 		group: {},// holds details of a group of elements which form a star rating widget
 		calls: 0,// differentiates groups of the same name to be created on separate plugin calls
@@ -75,54 +75,54 @@
 				$.rating.event.reset(gid, el, settings);
 				// click callback, as requested here: http://plugins.jquery.com/node/1655
 				if(settings.callback) settings.callback.apply($.rating.group[gid].valueElem[0], [val, lnk[0]]);
-			}      
+			}
 		}// plugin events
 	};
-	
+
 	$.fn.rating = function(instanceSettings){
 		if(this.length==0) return this; // quick fail
-		
+
 		instanceSettings = $.extend(
 			{}/* new object */,
 			$.rating/* global settings */,
 			instanceSettings || {} /* just-in-time settings */
 		);
-		
+
 		// increment plugin calls
 		$.rating.calls++;
-		
+
 		// loop through each matched element
 		this.each(function(i){
-			
+
 			var settings = $.extend(
 				{}/* new object */,
 				instanceSettings || {} /* current call settings */,
 				($.metadata? $(this).metadata(): ($.meta?$(this).data():null)) || {} /* metadata settings */
 			);
 			////if(window.console) console.log([this.name, settings.half, settings.split], '#');
-			
+
 			// Generate internal control ID
 			// - ignore square brackets in element names
 			var eid = (this.name || 'unnamed-rating').replace(/\[|\]+/g, "_");
-			
+
 			// differentiate groups of the same name on separate plugin calls
 			// SEE: http://code.google.com/p/jquery-star-rating-plugin/issues/detail?id=5
 			var gid = $.rating.calls +'_'+ eid;
-			
+
 			// Grouping
 			if(!$.rating.group[gid]) $.rating.group[gid] = {count: 0};
 			i = $.rating.group[gid].count; $.rating.group[gid].count++;
-			
+
 			// Accept readOnly setting from 'disabled' property
-			$.rating.group[gid].readOnly = $.rating.group[gid].readOnly || settings.readOnly || $(this).attr('disabled');
-			
+			$.rating.group[gid].readOnly = $.rating.group[gid].readOnly || settings.readOnly || $(this).prop('disabled');
+
 			// Things to do with the first element...
 			if(i == 0){
 				// Create value element (disabled if readOnly)
 				$.rating.group[gid].valueElem = $('<input type="hidden" name="' + eid + '" value=""' + (settings.readOnly ? ' disabled="disabled"' : '') + '/>');
 				// Insert value element into form
 				$(this).before($.rating.group[gid].valueElem);
-				
+
 				if($.rating.group[gid].readOnly || settings.required){
 					// DO NOT display 'cancel' button
 				}
@@ -136,14 +136,14 @@
 					);
 				}
 			}; // if (i == 0) (first element)
-			
+
 			// insert rating option right after preview element
 			eStar = $('<div class="star"><a title="' + (this.title || this.value) + '">' + this.value + '</a></div>');
 			$(this).after(eStar);
-			
+
 			// Half-stars?
 			if(settings.half) settings.split = 2;
-			
+
 			// Prepare division settings
 			if(typeof settings.split=='number' && settings.split>0){
 				var stw = ($.fn.width ? $(eStar).width() : 0) || settings.starWidth;
@@ -155,10 +155,10 @@
 				// this is work-around to IE's stupid box model (position:relative doesn't work)
 				.find('a').css({ 'margin-left':'-'+ (spi*spw) +'px' })
 			};
-			
+
 			// Remember group name so controls within the same container don't get mixed up
 			$(eStar).addClass('star_group_'+gid);
-			
+
 			// readOnly?
 			if($.rating.group[gid].readOnly)//{ //save a byte!
 				// Mark star as readOnly so user can customize display
@@ -173,18 +173,18 @@
 				.mouseout(function(){ $.rating.event.drain(gid, this, settings); $.rating.event.reset(gid, this, settings); })
 				.click(function(){ $.rating.event.click(gid, this, settings); });
 			//}; //save a byte!
-			
+
 			////if(window.console) console.log(['###', gid, this.checked, $.rating.group[gid].initial]);
 			if(this.checked) $.rating.group[gid].current = eStar;
-			
+
 			// remove this checkbox - values will be stored in a hidden field
 			$(this).remove();
-			
+
 			// reset display if last element
 			if(i + 1 == this.length) $.rating.event.reset(gid, this, settings);
-		
+
 		}); // each element
-			
+
 		// initialize groups...
 		for(gid in $.rating.group)//{ not needed, save a byte!
 			(function(c, v, gid){ if(!c) return;
@@ -193,21 +193,21 @@
 			})
 			($.rating.group[gid].current, $.rating.group[gid].valueElem, gid);
 		//}; not needed, save a byte!
-		
+
 		return this; // don't break the chain...
 	};
-	
-	
-	
+
+
+
 	/*
 		### Default implementation ###
 		The plugin will attach itself to file inputs
 		with the class 'multi' when the page loads
 	*/
 	$(function(){ $('input[type=radio].star').rating(); });
-	
-	
-	
+
+
+
 /*# AVOID COLLISIONS #*/
 })(jQuery);
 /*# AVOID COLLISIONS #*/

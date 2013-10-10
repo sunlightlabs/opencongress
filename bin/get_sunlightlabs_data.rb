f537136b0a251105c5d5c64f0304932afcf3af64
Dan Drinkard <dan.drinkard@gmail.com>
2013-10-10 14:49:27 -0400

    Deploying facebook login for testing...

--- bin/get_sunlightlabs_data.rb
+++ bin/get_sunlightlabs_data.rb
@@ -8,12 +8,12 @@ require 'open-uri'
 Person.all_sitting.each do |p|
   begin
     url = "http://services.sunlightlabs.com/api/legislators.get.xml?apikey=#{ApiKeys.sunlightlabs_key}&govtrack_id=#{p.id}"
-    puts "Getting Sublight Labs data for: #{p.name}"
-  
+    puts "Getting Sunlight Labs data for: #{p.name}"
+
     doc = REXML::Document.new open(url)
-  
+
     #puts "body: #{doc.to_s}"
-  
+
     p.website = doc.elements['response/legislator/website'].text if doc.elements['response/legislator/website']
     p.congress_office = doc.elements['response/legislator/congress_office'].text if doc.elements['response/legislator/congress_office']
     p.phone = doc.elements['response/legislator/phone'].text if doc.elements['response/legislator/phone']
@@ -21,7 +21,7 @@ Person.all_sitting.each do |p|
     p.contact_webform = doc.elements['response/legislator/webform'].text if doc.elements['response/legislator/webform']
     p.sunlight_nickname = doc.elements['response/legislator/nickname'].text if doc.elements['response/legislator/nickname']
 
-    p.save  
+    p.save
 
   rescue
     puts "ERROR WITH #{p}: #{$!}"

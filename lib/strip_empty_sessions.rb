21fde22833a8d44b17ba0fcc2541ff0884959fab
Dan Drinkard <dan.drinkard@gmail.com>
2013-10-25 12:49:45 -0400

    log after state

--- lib/strip_empty_sessions.rb
+++ lib/strip_empty_sessions.rb
@@ -26,6 +26,7 @@ class StripEmptySessions
     status, headers, body = @app.call(env)
 
     set_cookie_lines = headers.fetch('Set-Cookie', '').lines.map(&:strip).to_a
+    logger.write("Received set-cookie from backend: #{pp set_cookie_lines}, from headers: #{pp headers}")
 
     @logged_in_after_response = set_cookie_lines.select{ |c| c.starts_with?("#{@logged_in_cookie_name}=true") }.to_a.empty? == false
 

189d6e6b0fd716a62b35866baf1b9070be7cf6a4
Dan Drinkard <dan.drinkard@gmail.com>
2013-10-25 18:16:12 -0400

    update s3 bucket for blog post assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.s3_access_key_id = ApiKeys.aws_access_key_id
   config.s3_secret_access_key =  ApiKeys.aws_secret_access_key
-  config.s3_bucket = "a0.opencongress.org"
+  config.s3_bucket = "a4.opencongress.org"
   # Note: S3 uploads into "uploads" dir on the host.
 end

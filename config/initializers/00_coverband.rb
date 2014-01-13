60349a22e5abab41fd1826d0d7f768f0195769fb
Dan Drinkard <dan.drinkard@gmail.com>
2014-01-13 11:44:14 -0500

    higher coverband hitrate

--- config/initializers/00_coverband.rb
+++ config/initializers/00_coverband.rb
@@ -6,7 +6,7 @@ Coverband.configure do |config|
   config.coverage_baseline = JSON.parse(File.read('./tmp/coverband_baseline.json'))
   config.root_paths        = ['/app/']
   config.ignore            = ['vendor']
-  config.percentage        = 5.0
+  config.percentage        = 25.0
 end
 
 module OpenCongress

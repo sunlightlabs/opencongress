bc6de2238a86d383d48d81c36ee68a2364ed5e93
Dan Drinkard <dan.drinkard@gmail.com>
2014-01-13 11:50:20 -0500

    25 was too much!

--- config/initializers/00_coverband.rb
+++ config/initializers/00_coverband.rb
@@ -6,7 +6,7 @@ Coverband.configure do |config|
   config.coverage_baseline = JSON.parse(File.read('./tmp/coverband_baseline.json'))
   config.root_paths        = ['/app/']
   config.ignore            = ['vendor']
-  config.percentage        = 25.0
+  config.percentage        = 12.0
 end
 
 module OpenCongress

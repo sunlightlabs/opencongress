581df7cb541000268cf2915b655be750415f1675
Drew Vogel <dvogel@sunlightfoundation.com>
2014-02-18 14:35:56 -0500

    Added database migrations for new bill title fields.

---
+++ db/migrate/20140123223304_populate_manual_title_for_bills.rb
@@ -0,0 +1,14 @@
+class PopulateManualTitleForBills < ActiveRecord::Migration
+  def self.up
+    default_titles = BillTitle.where(:is_default => true).to_a
+    default_titles.each do |bill_title|
+      # Using update_all to avoid save handlers since we don't have
+      # update_column (added in rails 3.1)
+      Bill.update_all({:manual_title => bill_title.title}, ['id = ?', bill_title.bill.id])
+    end
+  end
+
+  def self.down
+    Bill.update_all({:manual_title => nil}, 'manual_title IS NOT NULL')
+  end
+end

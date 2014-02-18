04ec6c2c9f221e60ac60a51b6b03eb33dfa816e1
Drew Vogel <dvogel@sunlightfoundation.com>
2014-02-18 15:58:47 -0500

    Tweaked Bill.manual_title data migration.

--- db/migrate/20140123223304_populate_manual_title_for_bills.rb
+++ db/migrate/20140123223304_populate_manual_title_for_bills.rb
@@ -4,7 +4,7 @@ class PopulateManualTitleForBills < ActiveRecord::Migration
     default_titles.each do |bill_title|
       # Using update_all to avoid save handlers since we don't have
       # update_column (added in rails 3.1)
-      Bill.update_all({:manual_title => bill_title.title}, ['id = ?', bill_title.bill.id])
+      Bill.where(:id => bill_title.bill_id).update_all(:manual_title => bill_title.title)
     end
   end
 

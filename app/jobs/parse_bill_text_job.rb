047034ecd63378a8dfdc5573f15a0953383da69f
Drew Vogel <dvogel@sunlightfoundation.com>
2014-03-05 14:43:35 -0500

    Separated bill/congress paths for bill text parsing.

--- app/jobs/parse_bill_text_job.rb
+++ app/jobs/parse_bill_text_job.rb
@@ -3,30 +3,58 @@ require 'ostruct'
 require 'date'
 require 'yaml'
 require 'fileutils'
+require 'rexml/document'
 
 
 module ParseBillTextJob
-  include REXML
-
   def self.perform (options = {})
-    congress = options.fetch(:congress, Settings.default_congress).to_i
+    bill_id = options[:bill]
 
+    if bill_id.nil?
+      for_congress options.fetch(:congress, Settings.default_congress).to_i
+    else
+      for_bill bill_id
+    end
+  end
+
+  def self.for_congress (congress)
     Bill.get_types_ordered_new.each do |bill_type, bill_title_prefix|
       # We get the bill list from the database because even if we have a
       # text file for a bill, we won't ever display it unless it's also
       # in the database.
       bill_list = Bill.where(:bill_type => bill_type, :session => congress).order(:number).to_a
 
-      # Keep a file listing around to avoid globing the directory for each file.
-      bill_version_file_lookup = build_version_file_lookup(congress, bill_type)
-      bill_text_file_lookup = build_text_file_lookup(congress, bill_type)
+      unless bill_list.empty?
+        # Keep a file listing around to avoid globing the directory for each file.
 
-      puts "Processing #{bill_list.length} bills of type #{bill_type}"
-      bill_list.each do |bill|
-        version_files = bill_version_file_lookup.fetch(bill.ident, [])
-        text_files = bill_text_file_lookup.fetch(bill.ident, [])
-        parse_files bill, text_files, version_files, options
+        puts "Processing #{bill_list.length} bills of type #{bill_type}"
+        parse_bills bill_list,
+                    build_text_file_lookup(congress, bill_type),
+                    build_version_file_lookup(congress, bill_type),
+                    options
       end
+    end
+  end
+
+  def self.for_bill (bill_id, options = Hash.new)
+    puts "Parsing text for #{bill_id}"
+    bill_type, bill_number, congress = Bill.ident(bill_id)
+    bill_list = Bill.where(:number => bill_number, :bill_type => bill_type, :session => congress).to_a
+    parse_bills(bill_list,
+                build_text_file_lookup(congress, bill_type),
+                build_version_file_lookup(congress, bill_type),
+                options)
+  end
+
+
+  protected
+  def self.parse_bills (bill_list, text_file_lookup, version_file_lookup, options = Hash.new)
+    bill_list.each do |bill|
+      govtrack_bill_ident = "#{bill.reverse_abbrev_lookup}#{bill.number}-#{bill.session}"
+      parse_files(bill,
+                  text_file_lookup.fetch(govtrack_bill_ident, []),
+                  version_file_lookup.fetch(govtrack_bill_ident, []),
+                  options)
     end
   end
 
@@ -49,8 +77,8 @@ module ParseBillTextJob
     # For bills with version files, we use the "from-to" file as the text of
     # the "to" version of the bill. There will be no such version for the
     # initial version, so we use the text file for that version.
-    to_versions = version_files.map{ |ver| ver[:to_version] }
-    text_versions = text_files.map{ |txt| txt[:version] }
+    to_versions = version_files.map{ |ver| ver[:to_version] }.compact
+    text_versions = text_files.map{ |txt| txt[:version] }.compact
     initial_versions = text_versions - to_versions
     puts "Parsing files for #{bill.ident} :: #{initial_versions} :: #{to_versions}"
 
@@ -82,8 +110,7 @@ module ParseBillTextJob
     end
   end
 
-  # TODO: Should be private
-  def self.build_version_file_lookup (congress, bill_type)
+  def self.build_version_file_lookup (congress, bill_type, bill_number=nil)
     govtrack_bill_type = Bill.govtrack_lookup(bill_type)
     version_dir = File.join(Settings.govtrack_billtext_diff_path, congress.to_s, govtrack_bill_type)
     version_files = Dir.glob(File.join(version_dir, "#{govtrack_bill_type}*.xml"))
@@ -124,7 +151,6 @@ module ParseBillTextJob
     end
   end
 
-  # TODO: Should be private
   def self.parse_file (bill, text_version, input_path, output_path, options = {})
     file = File.open(input_path, 'r')
     file_timestamp = File.mtime(input_path)
@@ -157,148 +183,149 @@ module ParseBillTextJob
       doc.write outfile
     end
   end
-end
 
+  $node_order = 0
 
-$node_order = 0
+  def self.tree_walk(element, version, in_inline = false, in_removed = false)
+    removed = false
 
-def tree_walk(element, version, in_inline = false, in_removed = false)
-  removed = false
-
-  unless element.has_elements?
-    if element.name == 'p' and element.text.blank?
-      element.parent.delete(element)
-    end
-  end
-
-  element.elements.to_a.each do |e|
-    case e.name
-    when 'changed'
-       e.name = 'span'
-       e.attributes['class'] = 'bill_text_changed'
-    when 'changed-from'
-       e.name = 'span'
-       e.attributes['class'] = 'bill_text_changed_from'
-       e.attributes['style'] = 'display: none;'
-    when 'changed-to'
-       e.name = 'span'
-       e.attributes['class'] = 'bill_text_changed_to'
-    when 'inserted'
-      e.attributes['class'] = "bill_text_inserted"
-
-      e.name = in_inline ? 'span' : 'div'
-    when 'removed'
-      e.attributes['class'] = "bill_text_removed"
-      e.attributes['style'] = "display: none;"
-      removed = true
-
-      e.name = in_inline ? 'span' : 'div'
-    when 'p'
-      #e.name = 'span' if in_inline
-    when 'ul'
-      e.name = 'span' if in_inline
-    when 'h2','h3','h4'
-      if in_inline
-        e.name = 'span'
-        e.attributes['style'] = "font-size: 14px; font-weight:bold;"
+    unless element.has_elements?
+      if element.name == 'p' and element.text.blank?
+        element.parent.delete(element)
       end
     end
 
-    unless e.attributes['nid'].nil? or e.name == 'h2' or e.name == 'h3' or e.name == 'h4' or in_removed
-      e.attributes['class'] = 'bill_text_section'
-      e.attributes['id'] = "bill_text_section_#{e.attributes['nid']}"
-      e.attributes['onmouseover'] = "BillText.mouseOverSection('#{e.attributes['nid']}');"
-      e.attributes['onmouseout'] = "BillText.mouseOutSection('#{e.attributes['nid']}');"
+    element.elements.to_a.each do |e|
+      case e.name
+      when 'changed'
+         e.name = 'span'
+         e.attributes['class'] = 'bill_text_changed'
+      when 'changed-from'
+         e.name = 'span'
+         e.attributes['class'] = 'bill_text_changed_from'
+         e.attributes['style'] = 'display: none;'
+      when 'changed-to'
+         e.name = 'span'
+         e.attributes['class'] = 'bill_text_changed_to'
+      when 'inserted'
+        e.attributes['class'] = "bill_text_inserted"
 
-      menu = Element.new "span"
-      menu.attributes['class'] = 'bill_text_section_menu'
-      menu.attributes['id'] = "bill_text_section_menu_#{e.attributes['nid']}"
-      menu.attributes['style'] = 'display:none;'
+        e.name = in_inline ? 'span' : 'div'
+      when 'removed'
+        e.attributes['class'] = "bill_text_removed"
+        e.attributes['style'] = "display: none;"
+        removed = true
 
-      comments_show = Element.new "a"
-      comments_show.attributes['href'] = "#"
-      comments_show.attributes['id'] = "show_comments_link_#{e.attributes['nid']}"
-      comments_show.attributes['class'] = "small_button pushright"
-      comments_show.attributes['onClick'] = "BillText.showComments(#{version.id}, '#{e.attributes['nid']}'); return false;"
-      comments_show.text = ""
+        e.name = in_inline ? 'span' : 'div'
+      when 'p'
+        #e.name = 'span' if in_inline
+      when 'ul'
+        e.name = 'span' if in_inline
+      when 'h2','h3','h4'
+        if in_inline
+          e.name = 'span'
+          e.attributes['style'] = "font-size: 14px; font-weight:bold;"
+        end
+      end
 
-      comments_show_span = Element.new "span"
-      comments_show_span.text = "Comments"
+      unless e.attributes['nid'].nil? or e.name == 'h2' or e.name == 'h3' or e.name == 'h4' or in_removed
+        e.attributes['class'] = 'bill_text_section'
+        e.attributes['id'] = "bill_text_section_#{e.attributes['nid']}"
+        e.attributes['onmouseover'] = "BillText.mouseOverSection('#{e.attributes['nid']}');"
+        e.attributes['onmouseout'] = "BillText.mouseOutSection('#{e.attributes['nid']}');"
 
-      comments_show.elements << comments_show_span
+        menu = REXML::Element.new "span"
+        menu.attributes['class'] = 'bill_text_section_menu'
+        menu.attributes['id'] = "bill_text_section_menu_#{e.attributes['nid']}"
+        menu.attributes['style'] = 'display:none;'
 
-      comments_hide = Element.new "a"
-      comments_hide.attributes['href'] = "#"
-      comments_hide.attributes['id'] = "close_comments_link_#{e.attributes['nid']}"
-      comments_hide.attributes['class'] = "small_button pushright"
-      comments_hide.attributes['style'] = 'display:none;'
-      comments_hide.attributes['onClick'] = "BillText.closeComments(#{version.id}, '#{e.attributes['nid']}'); return false;"
-      comments_hide.text = ""
+        comments_show = REXML::Element.new "a"
+        comments_show.attributes['href'] = "#"
+        comments_show.attributes['id'] = "show_comments_link_#{e.attributes['nid']}"
+        comments_show.attributes['class'] = "small_button pushright"
+        comments_show.attributes['onClick'] = "BillText.showComments(#{version.id}, '#{e.attributes['nid']}'); return false;"
+        comments_show.text = ""
 
-      comments_hide_span = Element.new "span"
-      comments_hide_span.text = "Close Comments"
+        comments_show_span = REXML::Element.new "span"
+        comments_show_span.text = "Comments"
 
-      comments_hide.elements << comments_hide_span
+        comments_show.elements << comments_show_span
 
-      permalink = Element.new "a"
-      permalink.attributes['href'] = "?version=#{version.version}&nid=#{e.attributes['nid']}"
-      permalink.attributes['id'] = "permalink_#{e.attributes['nid']}"
-      permalink.attributes['class'] = "small_button"
-      permalink.text = ""
+        comments_hide = REXML::Element.new "a"
+        comments_hide.attributes['href'] = "#"
+        comments_hide.attributes['id'] = "close_comments_link_#{e.attributes['nid']}"
+        comments_hide.attributes['class'] = "small_button pushright"
+        comments_hide.attributes['style'] = 'display:none;'
+        comments_hide.attributes['onClick'] = "BillText.closeComments(#{version.id}, '#{e.attributes['nid']}'); return false;"
+        comments_hide.text = ""
 
-      permalink_span = Element.new "span"
-      permalink_span.text = "Permalink"
+        comments_hide_span = REXML::Element.new "span"
+        comments_hide_span.text = "Close Comments"
 
-      permalink.elements << permalink_span
+        comments_hide.elements << comments_hide_span
 
-      comments = Element.new "div"
-      comments.attributes['id'] = "bill_text_comments_#{e.attributes['nid']}"
-      comments.attributes['class'] = 'bill_text_section_comments'
-      comments.attributes['style'] = 'display:none;'
-      comments.text = ""
+        permalink = REXML::Element.new "a"
+        permalink.attributes['href'] = "?version=#{version.version}&nid=#{e.attributes['nid']}"
+        permalink.attributes['id'] = "permalink_#{e.attributes['nid']}"
+        permalink.attributes['class'] = "small_button"
+        permalink.text = ""
 
-      comments_clearer = Element.new "br"
-      comments_clearer.attributes['class'] = 'clear'
-      comments_clearer.text = ""
+        permalink_span = REXML::Element.new "span"
+        permalink_span.text = "Permalink"
 
-      comments.elements << comments_clearer
+        permalink.elements << permalink_span
 
-      img = Element.new "img"
-      img.attributes['style'] = 'margin: 5px; text-align: center;'
-      img.attributes['src'] = '/images/flat-loader.gif'
+        comments = REXML::Element.new "div"
+        comments.attributes['id'] = "bill_text_comments_#{e.attributes['nid']}"
+        comments.attributes['class'] = 'bill_text_section_comments'
+        comments.attributes['style'] = 'display:none;'
+        comments.text = ""
 
-      comments.elements << img
+        comments_clearer = REXML::Element.new "br"
+        comments_clearer.attributes['class'] = 'clear'
+        comments_clearer.text = ""
 
-      menu.elements << comments_show
-      menu.elements << comments_hide
-      menu.elements << permalink
+        comments.elements << comments_clearer
 
-      e.elements << menu
-      e.elements << comments
+        img = REXML::Element.new "img"
+        img.attributes['style'] = 'margin: 5px; text-align: center;'
+        img.attributes['src'] = '/images/flat-loader.gif'
+
+        comments.elements << img
+
+        menu.elements << comments_show
+        menu.elements << comments_hide
+        menu.elements << permalink
+
+        e.elements << menu
+        e.elements << comments
+      end
+
+      tree_walk(e, version, (in_inline or (e.name  =~ /p|em|h2|h3|h4/)), (in_removed or removed))
     end
+  end
 
-    tree_walk(e, version, (in_inline or (e.name  =~ /p|em|h2|h3|h4/)), (in_removed or removed))
+  def self.get_text_word_count (bill, text_version)
+    # get the word count from the text file
+    govtrack_bill_type = bill.reverse_abbrev_lookup
+    versioned_path = "#{Settings.govtrack_billtext_path}/#{bill.session}/#{govtrack_bill_type}/#{govtrack_bill_type}#{bill.number}#{text_version}.txt"
+    plain_path = "#{Settings.govtrack_billtext_path}/#{bill.session}/#{govtrack_bill_type}/#{govtrack_bill_type}#{bill.number}.txt"
+    path = File.exists?(versioned_path) ? versioned_path : plain_path
+    File.open(path) do |text_file|
+      raw_text = text_file.read
+
+      #remove line numbers
+      raw_text.gsub!(/^\s*\d+/, "")
+
+      word_count = raw_text.scan(/(\w|-)+/).size
+
+      raw_text = nil
+      text_file.close
+
+      return word_count
+    end
   end
 end
 
-def get_text_word_count (bill, text_version)
-  # get the word count from the text file
-  govtrack_bill_type = bill.reverse_abbrev_lookup
-  versioned_path = "#{Settings.govtrack_billtext_path}/#{bill.session}/#{govtrack_bill_type}/#{govtrack_bill_type}#{bill.number}#{text_version}.txt"
-  plain_path = "#{Settings.govtrack_billtext_path}/#{bill.session}/#{govtrack_bill_type}/#{govtrack_bill_type}#{bill.number}.txt"
-  path = File.exists?(versioned_path) ? versioned_path : plain_path
-  File.open(path) do |text_file|
-    raw_text = text_file.read
 
-    #remove line numbers
-    raw_text.gsub!(/^\s*\d+/, "")
-
-    word_count = raw_text.scan(/(\w|-)+/).size
-
-    raw_text = nil
-    text_file.close
-
-    return word_count
-  end
-end
 

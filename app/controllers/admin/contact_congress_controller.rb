33d49185ddca52b47a63507f580b0375f4236764
Dan Drinkard <dan.drinkard@gmail.com>
2014-05-09 15:28:19 -0400

    add stats since last update for contact-congress admin

--- app/controllers/admin/contact_congress_controller.rb
+++ app/controllers/admin/contact_congress_controller.rb
@@ -5,12 +5,12 @@ class Admin::ContactCongressController < Admin::IndexController
     @page_title = 'Contact Congress Configuration'
     @people = Person.all_sitting
   end
-  
+
   def letters
     @page_title = 'Contact Congress Letters'
     @letters = ContactCongressLetter.order('created_at DESC').all.paginate(:per_page => 50, :page => params[:page])
   end
-  
+
   def stats
     @page_title = 'Contact Congress Stats'
     @people = Person.all_sitting

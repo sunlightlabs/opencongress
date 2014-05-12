bb6056fa98687fa4a974334b9599d8162d1c9f36
Dan Drinkard <dan.drinkard@gmail.com>
2014-05-12 10:57:45 -0400

    reinstate delegations

--- app/models/user.rb
+++ app/models/user.rb
@@ -193,13 +193,13 @@ class User < ActiveRecord::Base
   # (After migration #20140424162910)
   ##
 
-  # %w(zipcode zip_four street_address street_address_2 small_picture main_picture
-  #    first_name last_name full_name website about city coordinates location address mobile_phone).each do |prop|
-  #   delegate prop.to_sym, :to => :user_profile
-  # end
-  # %w(comment_threshold opencongress_mail partner_mail sms_notifications email_notifications).each do |prop|
-  #   delegate prop.to_sym, :to => :user_options
-  # end
+  %w(zipcode zip_four street_address street_address_2 small_picture main_picture
+     first_name last_name full_name website about city coordinates location address mobile_phone).each do |prop|
+    delegate prop.to_sym, :to => :user_profile
+  end
+  %w(comment_threshold opencongress_mail partner_mail sms_notifications email_notifications).each do |prop|
+    delegate prop.to_sym, :to => :user_options
+  end
 
   %w(name email zipcode location profile actions tracked_items
      friends political_notebook watchdog).each do |prop|

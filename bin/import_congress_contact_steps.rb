1cf82739c00e2c1661328d3f45617c7185879405
Dan Drinkard <dan.drinkard@gmail.com>
2014-05-20 10:22:05 -0400

    oops undeclared var

--- bin/import_congress_contact_steps.rb
+++ bin/import_congress_contact_steps.rb
@@ -21,7 +21,7 @@ clone_path = File.join(Settings.data_path, 'contact-congress')
 datafile_path = File.join(clone_path, 'members')
 Person.legislator.each do |p|
   last_build = Person.formageddon_contact_steps.first.created_at rescue Time.new(0)
-  changed = `cd #{datafile_path} && git log -1 --since=#{last_run.iso8601} #{leg.bioguideid}.yaml`.present?
+  changed = `cd #{datafile_path} && git log -1 --since=#{last_build.iso8601} #{leg.bioguideid}.yaml`.present?
   if changed
     begin
       UnitedStates::ContactCongress.import_contact_steps_for p, "#{clone_path}/members/#{p.bioguideid}.yaml"

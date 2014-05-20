58e2df56b412ef1566543e02d242b35eaa0c2ab8
Dan Drinkard <dan.drinkard@gmail.com>
2014-05-20 11:37:07 -0400

    nope those are class methods

--- app/models/contact_congress_test.rb
+++ app/models/contact_congress_test.rb
@@ -18,19 +18,21 @@ class ContactCongressTest < ActiveRecord::Base
   scope :unknown, -> { where("status like ? or status like ?", "%WARNING%", "%START%") }
   scope :captcha_required, -> { where("status like ?", "%CAPTCHA_REQUIRED%") }
 
-  def recently_passed
-    latest.to_a.select{|t| t.status =~ /\ASENT\Z/ }
-  end
+  class << self
+    def recently_passed
+      latest.to_a.select{|t| t.status =~ /\ASENT\Z/ }
+    end
 
-  def recently_failed
-    latest.to_a.select{|t| t.status =~ /ERROR|SENT_AS_FAX/ }
-  end
+    def recently_failed
+      latest.to_a.select{|t| t.status =~ /ERROR|SENT_AS_FAX/ }
+    end
 
-  def recently_unknown
-    latest.to_a.select{|t| t.status =~ /WARNING/ }
-  end
+    def recently_unknown
+      latest.to_a.select{|t| t.status =~ /WARNING/ }
+    end
 
-  def recently_captcha_required
-    latest.to_a.select{|t| t.status =~ /CAPTCHA_REQUIRED/ }
+    def recently_captcha_required
+      latest.to_a.select{|t| t.status =~ /CAPTCHA_REQUIRED/ }
+    end
   end
 end

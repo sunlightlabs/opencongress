16057ecbcf791b3197d0a6d16e549f5ba63a34cd
Dan Drinkard <dan.drinkard@gmail.com>
2014-05-19 22:17:54 -0400

    fix typo

--- app/helpers/admin/contact_congress_letters_helper.rb
+++ app/helpers/admin/contact_congress_letters_helper.rb
@@ -30,7 +30,7 @@ module Admin::ContactCongressLettersHelper
     @@to_delete ||= AbandonedThreadsJob.to_destroy(:since => 2.months.ago, :remove => [:orphaned, :inactive])
     total_cnt = person.formageddon_threads.includes(:formageddon_letters).where("formageddon_threads.created_at >= ? and formageddon_threads.id not in(?)", person.formageddon_contact_steps.last.updated_at, @@to_delete).size rescue 0
     return '?' unless total_cnt > 0
-    succes_cnt = person.formageddon_threads.includes(:formageddon_letters).where("formageddon_threads.created_at >= ? and formageddon_letters.status = 'SENT'", person.formageddon_contact_steps.last.updated_at).size rescue 0
+    success_cnt = person.formageddon_threads.includes(:formageddon_letters).where("formageddon_threads.created_at >= ? and formageddon_letters.status = 'SENT'", person.formageddon_contact_steps.last.updated_at).size rescue 0
     number_to_percentage(success_cnt.to_f / total_cnt.to_f * 100)
   end
 

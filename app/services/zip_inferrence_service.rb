2b4d5b63d823d5af70110ca14e4598a358ebe269
Olivia Cheng <olivia@heyitsolivia.com>
2014-05-28 10:26:46 -0400

    fix zip checks for geocoder.ca

--- app/services/zip_inferrence_service.rb
+++ app/services/zip_inferrence_service.rb
@@ -3,9 +3,9 @@ class ZipInferrenceService
   def initialize (user_profile)
     if user_profile.zipcode.blank? || user_profile.zip_four.blank?
       result = MultiGeocoder.search(user_profile.mailing_address).first
-      unless result.nil? || result.zipcode.blank?
-        user_profile.zipcode = result.zipcode
-        unless result.zip4.blank?
+      unless result.nil? || result.postal_code.blank?
+        user_profile.zipcode = result.postal_code
+        unless (!result.respond_to?(:zip4)) || result.zip4.blank?
           user_profile.zip_four = result.zip4
         end
       end

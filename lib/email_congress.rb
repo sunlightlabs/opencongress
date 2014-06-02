9864bc7ab8e4c5660870cb04136fa4a40c1bf6f5
Drew Vogel <dvogel@sunlightfoundation.com>
2014-06-02 16:55:57 -0400

    Handle BCC header added by Postmark.

--- lib/email_congress.rb
+++ lib/email_congress.rb
@@ -303,6 +303,7 @@ module EmailCongress
     end
 
     def expand_special_addresses (sender_user, addresses)
+      inbound_address = Settings.to_hash['email_congress_inbound_address']
       addresses.flat_map do |addr|
         if addr =~ /^myreps@/
           if sender_user.nil?
@@ -310,6 +311,8 @@ module EmailCongress
           else
             next EmailCongress.email_addresses_for_people(sender_user.my_congress_members)
           end
+        elsif inbound_address.present? && addr.downcase == inbound_address.downcase
+          next []
         else
           next addr
         end

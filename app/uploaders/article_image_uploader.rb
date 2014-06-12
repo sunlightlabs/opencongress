cd9efa8ec4982ec8d1c3ecfb3445456e438e194c
Dan Drinkard <dan.drinkard@gmail.com>
2014-06-12 13:22:02 -0400

    update fog config

--- app/uploaders/article_image_uploader.rb
+++ app/uploaders/article_image_uploader.rb
@@ -5,10 +5,6 @@ class ArticleImageUploader < CarrierWave::Uploader::Base
   # Include RMagick support:
   include CarrierWave::RMagick
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :s3
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   # def store_dir

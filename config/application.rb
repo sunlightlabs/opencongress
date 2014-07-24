528afdaf8b17bf20eeaa006f6b09871aa9b1fd8d
crdunwel <clayton.dunwell@gmail.com>
2014-07-24 18:05:54 -0400

    White space removal

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,6 @@ module OpenCongress
                                 #{config.root}/app/services
                                 )
 
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]

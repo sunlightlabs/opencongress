4995d7e940d6ce5a4a4eb63de6175d7095fd9092
crdunwel <clayton.dunwell@gmail.com>
2014-08-22 10:11:13 -0400

    precompile assets fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,6 @@ This is a JavaScript asset manifest. It lists javascript
 files, which will get concatenated and served.
 ***/
 //= require jquery
-//= require jquery_ujs
 //= require beta/beta.js
+//= require_tree .
+

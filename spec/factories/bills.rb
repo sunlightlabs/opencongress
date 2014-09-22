4a2033152ba5c60e93385fc745bd350706452121
Corey Speisman <cpseisman@gmail.com>
2014-09-22 11:30:17 -0400

    added feature test structure

---
+++ spec/factories/bills.rb
@@ -0,0 +1,37 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :bill do
+    id 54050 
+    session 111 
+    bill_type "hr" 
+    number 45 
+    introduced 1231218000
+    sponsor_id 400350 
+    lastaction 1234155600 
+    rolls nil 
+    last_vote_date nil 
+    last_vote_where nil 
+    last_vote_roll nil 
+    last_speech nil 
+    pl nil 
+    topresident_date 1234159200
+    topresident_datetime "2009-02-09"
+    summary "Blair Holt's Firearm Licensing and Record of Sale ..."
+    plain_language_summary "The Blair Holt's Firearm Licensing and Record of S..." 
+    hot_bill_category_id nil 
+    updated "2013-07-26 02:20:47" 
+    page_views_count 1421649 
+    is_frontpage_hot false
+    news_article_count 333 
+    blog_article_count 4922 
+    caption 'Tacos Tacos Tacos Burritos Burritos Burritos' 
+    key_vote_category_id nil 
+    is_major nil 
+    top_subject_id 8580 
+    short_title nil 
+    popular_title nil 
+    official_title nil 
+    manual_title nil
+  end
+end
---
+++ spec/features/bills_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+feature "Bills", :type => :feature do
+  self.use_transactional_fixtures = false
+  scenario "User wants to view bill text"
+end

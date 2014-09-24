90baa6fe88d1b41a0f3a409590d5d6ebd35be838
Sam Handler <sam.handler@gmail.com>
2014-09-24 11:54:45 -0400

    Update migrations for rails 4
    update_all changed between Rails 2 and 4. It's no longer an ActiveRecord::Base
    method; it now lives in ActiveRecord::Relation. Previously, the update_all method
    allowed you to supply selection parameters  (e.g. WHERE should_update = true)
    along with new values for certain columns. Now, you run a Model.where method,
    then call update_all on the resulting ActiveRecord::Relation.
    
    Don't think this will have any side effects, since all of these migrations have
    already been run in the important places. Note that db/structure.sql has
    changed, though.

--- db/migrate/20130719202154_add_is_major_to_bill.rb
+++ db/migrate/20130719202154_add_is_major_to_bill.rb
@@ -1,7 +1,7 @@
 class AddIsMajorToBill < ActiveRecord::Migration
   def self.up
     add_column :bills, :is_major, :boolean
-    Bill.update_all "is_major = true", "hot_bill_category_id IS NOT NULL"
+    Bill.where('hot_bill_category_id IS NOT NULL').update_all(is_major: true)
   end
 
   def self.down

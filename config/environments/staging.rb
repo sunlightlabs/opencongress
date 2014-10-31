a401de91b1c0ba4a9a44eb29b3c1c6f5f8685b12
Sam Handler <sam.handler@gmail.com>
2014-10-31 12:50:36 -0400

    don't serve static assets on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,7 +5,7 @@ OpenCongress::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier

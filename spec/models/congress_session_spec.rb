f47eb6012364aa99cd4f4514e50a23e78ec70605
Cspeisman <Cspeisman@gmail.com>
2014-12-09 15:28:08 -0500

    added a before each filter for congress_session

--- spec/models/congress_session_spec.rb
+++ spec/models/congress_session_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe CongressSession do
+  before(:each) { @session = FactoryGirl.create(:congress_session) }
+  
   describe "senate_session" do
-    session = FactoryGirl.create(:congress_session)
-
     it "return the current senate sessionate" do      
-      expect(CongressSession.senate_session).to eq session
+      expect(CongressSession.senate_session).to eq @session
     end
   end
+  
 end

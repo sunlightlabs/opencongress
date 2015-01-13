d23af9eb27c01548a228ae0e6fc14c3e5cac4c1a
Sam Handler <sam.handler@gmail.com>
2015-01-13 10:54:49 -0500

    Use integer in id lookup in daily_bioguideize_govtrack_images

--- bin/daily/daily_bioguideize_govtrack_images.rb
+++ bin/daily/daily_bioguideize_govtrack_images.rb
@@ -10,18 +10,20 @@ else
 end
 
 found_people = 0
-found_people_without_bioguides = 0
 missing_people = 0
 
 jpgs = Dir["#{Settings.data_path}/govtrack/photos/*.jpeg"]
 jpgs.each do |jpg|
   id = jpg.match(/\/([^\/]*).jpeg/).captures.first
   begin
-    person = Person.find(id)
+    person = Person.find(id.to_i)
     found_people = found_people + 1
     `cp -n /#{Settings.data_path}/govtrack/photos/#{id}.jpeg /#{Settings.data_path}/legislator_images/#{bioguideid}.jpeg`
   rescue
-    OCLogger.log "person not found: #{id}"
+    OCLogger.log "person not found from GovTrack image id: #{id}"
     missing_people = missing_people + 1
   end
 end
+
+OCLogger.log "found #{found_people} People in GovTrack photo import"
+OCLogger.log "missing #{missing_people} People in GovTrack photo import"

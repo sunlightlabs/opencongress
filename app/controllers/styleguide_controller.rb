35bdc48c03c2843820c2a0de6ea9c2e86c0b3b11
Olivia Cheng <olivia@heyitsolivia.com>
2015-01-15 17:27:43 -0500

    Creating additional pages for the pattern library

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -1,9 +1,19 @@
 class StyleguideController < ApplicationController
   layout 'new_design'
-  
+
   def show
   end
 
   def globals
   end
+
+  def forms
+  end
+
+  def cards
+  end
+
+  def navigation
+  end
+
 end

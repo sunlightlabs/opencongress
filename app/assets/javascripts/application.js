14307cd1c3ba0b389e25485a121602f3bbb88bf8
Cspeisman <Cspeisman@gmail.com>
2015-01-15 16:03:11 -0500

    more flexy and responsive globals page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@ This is a JavaScript asset manifest. It lists javascript
 files, which will get concatenated and served.
 ***/
 //= require jquery
+//= require bootstrap-sprockets.js
 //= require jquery_ujs
 //= require ./libraries/swfobject.js
 //= require_tree ./beta
-//= require bootstrap-sprockets

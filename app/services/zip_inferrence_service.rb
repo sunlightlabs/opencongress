cf774c431241f60452aacb4c8aa6ae976afa49dc
crdunwel <clayton.dunwell@gmail.com>
2015-01-29 12:15:59 -0500

    convenience methods / reorganizing logic

--- app/services/zip_inferrence_service.rb
+++ app/services/zip_inferrence_service.rb
@@ -6,37 +6,43 @@ class ZipInferrenceService
   #
   # @param user_profile [UserProfile] instance of UserProfile
   # @return
-  def initialize (user_profile)
+  def initialize(user_profile, force_update = false)
 
-    # try to resolve zip first with address using USPS server
-    if user_profile.street_address.present? and user_profile.city.present? and user_profile.user.state.present?
-      # grab data as 2 item array
-      zip5_zip4 = usps_zip_lookup(user_profile.street_address, user_profile.city, user_profile.user.state)
+    # only update if empty zip5/zip4/forcing an update
+    if user_profile.zipcode.blank? or user_profile.zip_four.blank? or force_update
 
-      # validation for zip5
-      if zip5_zip4[0].match(/\d{5}/) and user_profile.zipcode.blank?
-        user_profile.zipcode = zip5_zip4[0]
-      end
+      # try to resolve zip first with address using USPS server
+      if user_profile.street_address.present? and user_profile.city.present? and user_profile.user.state.present?
+        # grab data as 2 item array
+        zip5_zip4 = usps_zip_lookup(user_profile.street_address, user_profile.city, user_profile.user.state)
 
-      # validation for zip4
-      if zip5_zip4[1].match(/\d{4}/) and user_profile.zip_four.blank?
-        user_profile.zip_four = zip5_zip4[1]
-      end
-    end
+        # validation for zip5 and set
+        if zip5_zip4[0].valid_zip5?
+          user_profile.zipcode = zip5_zip4[0]
+        end
 
-    # fall back to geocoder if USPS method fails
-    if user_profile.zipcode.blank? or user_profile.zip_four.blank?
-      result = geocoder_zip_lookup(user_profile.mailing_address)
-
-      unless result.nil? or result.postal_code.blank?
-        user_profile.zipcode = result.postal_code
-        if result.respond_to?(:zip4) and not result.zip4.blank?
-          user_profile.zip_four = result.zip4
+        # validation for zip4 and set
+        if zip5_zip4[1].valid_zip4?
+          user_profile.zip_four = zip5_zip4[1]
         end
       end
+
+      # fall back to geocoder if USPS method fails
+      if user_profile.zipcode.blank? or user_profile.zip_four.blank?
+        result = geocoder_zip_lookup(user_profile.mailing_address)
+
+        unless result.nil? or result.postal_code.blank?
+          user_profile.zipcode = result.postal_code
+          if result.respond_to?(:zip4) and not result.zip4.blank?
+            user_profile.zip_four = result.zip4
+          end
+        end
+      end
+
+      # save data to UserProfile
+      user_profile.save!
     end
 
-    user_profile.save!
   end
 
 
@@ -49,7 +55,7 @@ class ZipInferrenceService
   def self.zip5_lookup(street_address, city, state)
     zip5_zip4 = usps_zip_lookup(street_address, city, state)
 
-    if zip5_zip4[0].match(/\d{5}/)
+    if zip5_zip4[0].valid_zip5?
       return zip5_zip4[0]
     end
 
@@ -57,6 +63,7 @@ class ZipInferrenceService
     if not result.nil? or not result.postal_code.blank?
       return result.postal_code
     end
+
   end
 
   # Looks up zip4 from partial address information
@@ -69,7 +76,7 @@ class ZipInferrenceService
   def self.zip4_lookup(street_address, city, state, zip5 = '')
     zip5_zip4 = usps_zip_lookup(street_address, city, state)
 
-    if zip5_zip4[1].match(/\d{4}/)
+    if zip5_zip4[1].valid_zip4?
       return zip5_zip4[1]
     end
 
@@ -89,12 +96,11 @@ class ZipInferrenceService
   def self.lookup(street_address, city, state, zip5 = '')
     zip5_zip4 = usps_zip_lookup(street_address, city, state)
 
-    if zip5_zip4[0].match(/\d{5}/) and zip5_zip4[1].match(/\d{4}/)
+    if zip5_zip4[0].valid_zip5? and zip5_zip4[1].valid_zip4?
       return zip5_zip4
     end
 
     result = geocoder_zip_lookup(street_address + ',' + city + ',' + state + ' ' + zip5)
-
     if not result.nil? or not result.postal_code.blank? and (result.respond_to?(:zip4) and not result.zip4.blank?)
       return [result.postal_code, result.zip4]
     end

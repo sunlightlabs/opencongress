7017d4a426beb9989f827ad9263b4131bf760878
Sam Handler <sam.handler@gmail.com>
2015-02-10 12:23:11 -0500

    Add basic filtering system based on scopes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -572,6 +572,16 @@ class ApplicationController < ActionController::Base
     @meta_tags_for_search.merge!(options)
   end
 
+  def filtering_defaults
+    model = controller_name.classify.constantize
+    model.filterable_fields[:basic].reject{ |k,v| v.nil? }
+  end
+
+  def filtering_params(filter_defaults=filtering_defaults)
+    model = controller_name.classify.constantize
+    filter_defaults.merge(params).slice(*model.filterable_fields[:basic].keys)
+  end
+
   # Set instance variable for page title, defaulting to "OpenCongress"
   # This instance variable is rendered in the main application layout.
   #

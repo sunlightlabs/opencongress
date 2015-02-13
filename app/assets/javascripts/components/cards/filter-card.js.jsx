c2a344146aa11681ba296c7687e828d1c2ffcca7
Cspeisman <Cspeisman@gmail.com>
2015-02-13 16:01:32 -0500

    search bar for mock up

--- app/assets/javascripts/components/cards/filter-card.js.jsx
+++ app/assets/javascripts/components/cards/filter-card.js.jsx
@@ -12,12 +12,18 @@ var FilterCard  = React.createClass({
 
     render: function () {
         return (
-            <div className="contents__side">
-              <form ref="form" method="get" action={this.props.url} onSubmit={this.formHandler}>
-                <input type="text" name="sponsor_id" ref="sponsor_id" />
-                <input type="submit" />
+            <aside className="filter">
+              <form className="filter__search" role="search" ref="form" action={this.props.url} onSubmit={this.formHandler} >
+                <div className="filter__section">
+                  <div className="input-group">
+                    <input type="text" className="filter__search-input" name="sponsor_id" ref="sponsor_id" placeholder="Search Activity Feed" />
+                      <span className="input-group-btn">
+                        <button className="filter__search-button"><span className="glyphicon glyphicon-search" onClick={this.formHandler} ></span></button>
+                      </span>
+                  </div>
+                </div>
               </form>
-            </div>
+            </aside>
         );
     }
 });

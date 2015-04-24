ef1e2292937ddefff36e507782c9b377e8fa3248
Cspeisman <Cspeisman@gmail.com>
2015-04-24 13:59:58 -0400

    setting content-disposition

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -115,7 +115,7 @@ class ProfileController < ApplicationController
      @user.senator_bookmarks.length > 0 ? @sen_items_tracked = Person.find_changes_since_for_senators_tracked(@user) : @sen_items_tracked = []
      respond_to do |format| 
        format.html
-       format.csv { render text: to_csv(@user)}
+       format.csv { render text: to_csv(@user) }
      end
   end
 
